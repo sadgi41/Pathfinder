59e2425a6a7641663a9bd86db5a930f8f6032978
Sadgi Jain <sadgijain41@gmail.com>
2026-04-08 16:44:43 +0530

    use of persist along with zustand

--- src/store/usePathfinderStore.js
+++ src/store/usePathfinderStore.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const usePathfinderStore = create((set) => ({
-    currentRole: null,
-    targetRole: null,
-    setCurrentRole: (role) => set({ currentRole: role }),
-    setTargetRole: (role) => set({ targetRole: role }),
-    reset: () => set({ currentRole: null, targetRole: null })
-}));
+const usePathfinderStore = create(
+  persist(
+    (set) => ({
+      currentRole: null,
+      targetRole: null,
 
-export { usePathfinderStore }
+      setCurrentRole: (role) => set({ currentRole: role }),
+      setTargetRole: (role) => set({ targetRole: role }),
+
+      reset: () => set({ currentRole: null, targetRole: null })
+    }),
+    {
+      name: "pathfinder-storage" // key in localStorage
+    }
+  )
+);
+
+export { usePathfinderStore };
